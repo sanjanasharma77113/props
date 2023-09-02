0b40a070c5187a59da892c4acdc176bb4c4d7c23
sanjana <126218981+sanjanasharma77113@users.noreply.github.com>
2023-09-02 12:25:15 +0530

    props

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+    <Header
+    src="https://e1.pxfuel.com/desktop-wallpaper/926/504/desktop-wallpaper-beautiful-and-cute-dolls-girl-doll.jpg"
+    width="400px"
+    height="400px"
+    />
+
+<Header
+    src="https://e1.pxfuel.com/desktop-wallpaper/155/34/desktop-wallpaper-very-cute-doll-for-facebook-cave-girls-dp-873x1297-for-your-mobile-tablet-cute-dp-thumbnail.jpg"
+    width="400px"
+    height="400px"
+    />
+
+<Header
+    
+    src="https://e0.pxfuel.com/wallpapers/440/472/desktop-wallpaper-cute-baby-barbie-baby-doll-thumbnail.jpg"
+    width="400px"
+    height="400px"
+    />"
+   
+     </div>
+   
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+function Header(props)  {
+
+
+    return (
+        <div>
+          <img src={props.src} width={props.width} height={props.height} />
+            </div>
+      )
+    }
+    
+
+ 
+export default Header
